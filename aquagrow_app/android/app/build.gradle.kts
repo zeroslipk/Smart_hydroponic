be4fb2c4d8d41b7c4240e6fa80575c9d841fc641
Youssef <YoussefHabil711@gmail.com>
2025-12-19 22:32:00 +0200

    Fix: Enable Core Library Desugaring for flutter_local_notifications

--- aquagrow_app/android/app/build.gradle.kts
+++ aquagrow_app/android/app/build.gradle.kts
@@ -16,10 +16,15 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+    
+    dependencies {
+        coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     }
 
     defaultConfig {
